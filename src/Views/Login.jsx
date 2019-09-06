e24d654c5895839fe8bcf56eace36fa33e4941bf
efyguevara <efyguevara@gmail.com>
2019-09-05 19:55:04 -0700

    Adding state User and redirect to Wall

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect} from 'react-router-dom'
 // import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import '../App.css';
@@ -7,19 +8,26 @@ import withFirebaseAuth from 'react-with-firebase-auth';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from '../initFirebase';
-// import Wall from './Wall';
+import Wall from './Wall';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       user: false,
     }
   }
+
+  redirectWall() {
+    return <Redirect to="/Wall"/>
+  }
+
   render() {
+
     const { user, signOut, signInWithGoogle } = this.props;
+    if (user) this.redirectWall()
     return (
       // <div>
       //   {
@@ -41,7 +49,7 @@ class Login extends Component {
                   <Form.Label>Email</Form.Label>
                   <Form.Control type="email" placeholder="Enter email" />
                   <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.  
+                    We'll never share your email with anyone else.
                   </Form.Text>
                 </Form.Group>
 
@@ -53,19 +61,10 @@ class Login extends Component {
                   <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button>Entrar</Button>
-  
-                <div>
-                  {
-                    user ? <p>Hello, {user.displayName}</p>
-                      : <p>Please, sign in</p>
-                  }
-                  {
-                    user ? <Button onClick={signOut}>Sign out</Button>
 
-                    : <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-                    
-                  }
-                </div>
+
+                <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+
               </Col>
             </Row>
           </Form>
